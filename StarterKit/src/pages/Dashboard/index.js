f2902a512d2a161abe58f10bf789dd52294d4c44
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2021-01-20 09:43:38 +0100

    WIP: Dashboard
    Signed-off-by: Ekojoka Chris Idakwo <chris.idakwo@gmail.com>

--- StarterKit/src/pages/Dashboard/index.js
+++ StarterKit/src/pages/Dashboard/index.js
@@ -22,7 +22,7 @@ class StarterPage extends Component {
                             <Card>
                                 <CardBody>
                                     <div>
-                                        <i className="ri-survey-line" style={{fontSize: "38px", fontWeight: "regular"}}></i>
+                                        <i className="ri-survey-line" style={{fontSize: "38px", fontWeight: "regular"}}/>
                                         <h6 className="font-weight-bolder">Inventory</h6>
                                         <p className="mb-2" style={{fontSize: "12px"}}>Inventory record and schedule for all battle assets and equipment</p>
                                     </div>
@@ -34,7 +34,7 @@ class StarterPage extends Component {
                             <Card>
                               <CardBody>
                                 <div>
-                                  <i className="ri-truck-line" style={{fontSize: "38px", fontWeight: "regular"}}></i>
+                                  <i className="ri-truck-line" style={{fontSize: "38px", fontWeight: "regular"}}/>
                                   <h6 className="font-weight-bolder">Vehicle/Equipment</h6>
                                   <p className="mb-2" style={{fontSize: "12px"}}>Track maintenance and battle-worthiness of vehicles & equipment</p>
                                 </div>
@@ -46,7 +46,7 @@ class StarterPage extends Component {
                             <Card>
                               <CardBody>
                                 <div>
-                                  <i className="ri-nurse-line" style={{fontSize: "38px", fontWeight: "regular"}}></i>
+                                  <i className="ri-nurse-line" style={{fontSize: "38px", fontWeight: "regular"}}/>
                                   <h6 className="font-weight-bolder">Technicians</h6>
                                   <p className="mb-2" style={{fontSize: "12px"}}>Manage Technicians and their maintenance activities on assets</p>
                                 </div>
@@ -68,7 +68,27 @@ class StarterPage extends Component {
                               <Col md={12} lg={6}>
                                 <Card>
                                   <CardBody>
-                                    <p>Hello</p>
+                                    <div className="d-flex d-inline-flex align-items-center mb-3">
+                                      <div className="bg-success mr-2" style={{height: "10px", width: "10px", borderRadius: "50%"}}/>
+                                      <h6 className="m-0" style={{fontWeight: "bold"}}>Battle Worthiness Status</h6>
+                                    </div>
+
+                                    <table className="small-datatable">
+                                      <tbody>
+                                        <tr style={{background: "rgba(116,120,141,0.2)"}}>
+                                          <td>Total Count</td>
+                                          <td>11397</td>
+                                        </tr>
+                                        <tr style={{background: "rgb(69,176,192,0.2)"}}>
+                                          <td>On Hand</td>
+                                          <td>101</td>
+                                        </tr>
+                                        <tr style={{background: "rgba(252,185,44,0.2)"}}>
+                                          <td>On Loan</td>
+                                          <td>6(0.8%)</td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
                                   </CardBody>
                                 </Card>
                               </Col>
@@ -76,7 +96,27 @@ class StarterPage extends Component {
                               <Col md={12} lg={6}>
                                 <Card>
                                   <CardBody>
-                                    <p>Hello</p>
+                                    <div className="d-flex d-inline-flex align-items-center mb-3">
+                                      <div className="bg-danger mr-2" style={{height: "10px", width: "10px", borderRadius: "50%"}}/>
+                                      <h6 className="m-0" style={{fontWeight: "bold"}}>Serviceable Status</h6>
+                                    </div>
+
+                                    <table className="small-datatable">
+                                      <tbody>
+                                      <tr style={{background: "rgba(255,61,96,0.2)"}}>
+                                        <td>Item Lines</td>
+                                        <td>11397</td>
+                                      </tr>
+                                      <tr style={{background: "rgb(52,58,64,0.2)"}}>
+                                        <td>Low Stock</td>
+                                        <td>0</td>
+                                      </tr>
+                                      <tr style={{background: "rgba(74,163,255,0.2)"}}>
+                                        <td>No Stock</td>
+                                        <td>6(0.8%)</td>
+                                      </tr>
+                                      </tbody>
+                                    </table>
                                   </CardBody>
                                 </Card>
                               </Col>
